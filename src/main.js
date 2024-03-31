501bea7a017c932ab32cf6660d47ec5a0af87b43
Thiago Souza <62295808+Mctks2@users.noreply.github.com>
2024-03-30 22:15:40 -0300

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,41 @@
+// Variáveis
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
+// Eventos
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", handleResetClick);
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Enter") {
+    handleResetClick();
+  }
+})
+
+// Funções callback
+function handleTryClick(event) {
+  event.preventDefault(); //previne o comportamento padrao do evento (form)
+
+  const inputNumber = document.querySelector("#inputNumber");
+
+  if (Number(inputNumber.value) == randomNumber) {
+    toggleScreen();
+    screen2.querySelector("h2").innerText = `Parabéns, você acertou em ${xAttempts === 1 ? "uma tentativa" : xAttempts + " tentativas" }.`;
+  }
+
+  inputNumber.value = "";
+  xAttempts++;
+}
+
+function handleResetClick() {
+  toggleScreen();
+  xAttempts = 1;
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+}
